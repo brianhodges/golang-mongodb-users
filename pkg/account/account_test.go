c39b2d481b024fa05c4069521baadc73d41c7566
Development <Development@Brian-N-Hodgess-MacBook.local>
2017-07-19 23:49:07 -0400

    add more tests

--- pkg/account/account_test.go
+++ pkg/account/account_test.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"bytes"
+	"fmt"
 	"golang-mongodb-users/pkg/util"
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
@@ -11,19 +12,19 @@ import (
 	"testing"
 )
 
-func TestAuthStatus(t *testing.T) {
-	//create db test user entry
-	var salt string = util.GenerateSalt()
-	var testuser = &User{
-		Username:             "TestUser",
-		FirstName:            "Test",
-		LastName:             "User",
-		Password:             "secretpassword",
-		PasswordConfirmation: "secretpassword",
-		PasswordSalt:         salt,
-		PasswordHash:         util.Encrypt(salt, "secretpassword"),
-	}
+//create db test user entry
+var salt string = util.GenerateSalt()
+var testuser = &User{
+	Username:             "TestUser",
+	FirstName:            "Test",
+	LastName:             "User",
+	Password:             "secretpassword",
+	PasswordConfirmation: "secretpassword",
+	PasswordSalt:         salt,
+	PasswordHash:         util.Encrypt(salt, "secretpassword"),
+}
 
+func TestAuthStatus(t *testing.T) {
 	//connect to mongodb
 	session := util.GetMongoDBSession()
 	defer session.Close()
@@ -51,5 +52,37 @@ func TestAuthStatus(t *testing.T) {
 	if status := rr.Code; status != http.StatusSeeOther {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusSeeOther)
+	}
+}
+
+func TestAuthHandlerResponse(t *testing.T) {
+	//connect to mongodb
+	session := util.GetMongoDBSession()
+	defer session.Close()
+	c := session.DB(os.Getenv("MONGODB_DB")).C(COLLECTION)
+	cnt, err := c.Find(bson.M{"username": testuser.Username}).Count()
+	util.CheckError(err)
+	if cnt == 0 {
+		err := c.Insert(testuser)
+		util.CheckError(err)
+	}
+
+	data := url.Values{}
+	data.Add("username", testuser.Username)
+	data.Add("password", testuser.Password)
+	b := bytes.NewBuffer([]byte(data.Encode()))
+	req, err := http.NewRequest("POST", "/auth", b)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(auth)
+	handler.ServeHTTP(rr, req)
+
+	expected := ""
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
 	}
 }
--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -10,3 +10,24 @@ func TestSaltGeneration(t *testing.T) {
 		t.Errorf("Incorrect Salt length from GenerateSalt()")
 	}
 }
+
+func TestEncryptionFailure(t *testing.T) {
+	secret := "secretpassphrase"
+	salt := GenerateSalt()
+	password := Encrypt(salt, secret)
+	salt2 := GenerateSalt()
+	password2 := Encrypt(salt2, secret)
+	if password == password2 {
+		t.Errorf("Encrypted password should not match...")
+	}
+}
+
+func TestEncryptionSuccess(t *testing.T) {
+	secret := "secretpassphrase"
+	salt := GenerateSalt()
+	password := Encrypt(salt, secret)
+	password2 := Encrypt(salt, secret)
+	if password != password2 {
+		t.Errorf("Encryption match failed...")
+	}
+}
