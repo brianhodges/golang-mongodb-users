4bfb9a9145596c3f4b79a93b4656901446c2b49a
Development <Development@Brian-N-Hodgess-MacBook.local>
2017-07-29 16:05:37 -0400

    more cleanup

--- pkg/account/account.go
+++ pkg/account/account.go
@@ -89,36 +89,32 @@ func edit(w http.ResponseWriter, r *http.Request) {
 //POST -> /register
 func create(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		var username string = r.PostFormValue("username")
-		var firstName string = r.PostFormValue("first_name")
-		var lastName string = r.PostFormValue("last_name")
-		var password string = r.PostFormValue("password")
-		var passwordConfirmation string = r.PostFormValue("password_confirmation")
+		var salt string = util.GenerateSalt()
+		var username, firstName, lastName, password, passwordConfirmation string = r.PostFormValue("username"), r.PostFormValue("first_name"),
+			r.PostFormValue("last_name"), r.PostFormValue("password"),
+			r.PostFormValue("password_confirmation")
+		var u = &User{
+			Username:             username,
+			FirstName:            firstName,
+			LastName:             lastName,
+			Password:             password,
+			PasswordConfirmation: passwordConfirmation,
+			PasswordSalt:         salt,
+			PasswordHash:         util.Encrypt(salt, password),
+		}
 
-		//connect to mongodb
 		session := util.GetMongoDBSession()
 		defer session.Close()
 		c := session.DB(os.Getenv("MONGODB_DB")).C(COLLECTION)
 
-		//find user and create if not exist
 		cnt, err := c.Find(bson.M{"username": username}).Count()
 		util.CheckError(err)
 		if cnt == 0 {
-			var salt string = util.GenerateSalt()
-			var newuser = &User{
-				Username:             username,
-				FirstName:            firstName,
-				LastName:             lastName,
-				Password:             password,
-				PasswordConfirmation: passwordConfirmation,
-				PasswordSalt:         salt,
-				PasswordHash:         util.Encrypt(salt, password),
-			}
-			if newuser.Validate() == false {
-				data := TemplateVars{App: util.App, Message: "", Errors: newuser.Errors}
+			if u.Validate() == false {
+				data := TemplateVars{App: util.App, Message: "", Errors: u.Errors}
 				util.Render(w, "templates/register.html", data)
 			} else {
-				err = c.Insert(newuser)
+				err = c.Insert(u)
 				util.CheckError(err)
 				data := TemplateVars{App: util.App, Message: "User Successfully Created. Login.", Errors: nil}
 				util.Render(w, "templates/login.html", data)
@@ -139,12 +135,10 @@ func auth(w http.ResponseWriter, r *http.Request) {
 		var username string = r.PostFormValue("username")
 		var password string = r.PostFormValue("password")
 
-		//connect to mongodb
 		session := util.GetMongoDBSession()
 		defer session.Close()
 		c := session.DB(os.Getenv("MONGODB_DB")).C(COLLECTION)
 
-		//find user and check password
 		err := c.Find(bson.M{"username": username}).One(&result)
 		if result.PasswordHash == util.Encrypt(result.PasswordSalt, password) {
 			util.CheckError(err)
@@ -161,39 +155,36 @@ func auth(w http.ResponseWriter, r *http.Request) {
 //POST -> /edit
 func update(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+		var salt string = util.GenerateSalt()
 		var result = AuthenticatedUser(r)
-		var username string = r.PostFormValue("username")
-		var firstName string = r.PostFormValue("first_name")
-		var lastName string = r.PostFormValue("last_name")
-		var password string = r.PostFormValue("password")
-		var passwordConfirmation string = r.PostFormValue("password_confirmation")
+		var username, firstName, lastName, password, passwordConfirmation string = r.PostFormValue("username"), r.PostFormValue("first_name"),
+			r.PostFormValue("last_name"), r.PostFormValue("password"),
+			r.PostFormValue("password_confirmation")
+		var u = User{
+			Username:             username,
+			FirstName:            firstName,
+			LastName:             lastName,
+			Password:             password,
+			PasswordConfirmation: passwordConfirmation,
+			PasswordSalt:         salt,
+			PasswordHash:         util.Encrypt(salt, password),
+		}
 
-		//connect to mongodb
 		session := util.GetMongoDBSession()
 		defer session.Close()
 		c := session.DB(os.Getenv("MONGODB_DB")).C(COLLECTION)
 
 		if result.Username != "" {
-			var salt string = util.GenerateSalt()
-			var updUser User
-			updUser.Username = username
-			updUser.FirstName = firstName
-			updUser.LastName = lastName
-			updUser.Password = password
-			updUser.PasswordConfirmation = passwordConfirmation
-			updUser.PasswordSalt = salt
-			updUser.PasswordHash = util.Encrypt(salt, password)
-
-			if updUser.Validate() == false {
-				data := TemplateVars{App: util.App, Message: "", Errors: updUser.Errors, Account: result}
+			if u.Validate() == false {
+				data := TemplateVars{App: util.App, Message: "", Errors: u.Errors, Account: result}
 				util.Render(w, "templates/edit.html", data)
 			} else {
-				change := bson.M{"$set": bson.M{"first_name": updUser.FirstName,
-					"last_name": updUser.LastName, "password_salt": updUser.PasswordSalt,
-					"password_hash": updUser.PasswordHash}}
+				change := bson.M{"$set": bson.M{"first_name": u.FirstName,
+					"last_name": u.LastName, "password_salt": u.PasswordSalt,
+					"password_hash": u.PasswordHash}}
 				err := c.Update(result, change)
 				util.CheckError(err)
-				token := createToken(updUser)
+				token := createToken(u)
 				util.SetSession(token, w)
 				http.Redirect(w, r, "/", http.StatusSeeOther)
 			}
@@ -227,7 +218,6 @@ func AuthenticatedUser(r *http.Request) User {
 	token := util.GetTokenFromSession(r)
 	if token != "" {
 		username := parseToken(token)
-		//connect to mongodb
 		session := util.GetMongoDBSession()
 		defer session.Close()
 		c := session.DB(os.Getenv("MONGODB_DB")).C(COLLECTION)
